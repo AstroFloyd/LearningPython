a1cd314b9456c3127920e81858963f15b0d72c34
AstroFloyd <AstroFloyd@gmail.com>
2015-12-07 16:37:43 +0100

    Added plot_legend.py to create a plot with a legend

---
+++ plot_legend.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+## @file plot_legend.py
+#  Python example for using a legend in a plot using matplotlib
+#
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+## x array
+x = np.arange(0.0, 2.0, 0.01)               # Array 0-2 with steps of 0.01
+
+## Sine array
+ys = np.sin(2*np.pi*x)                      # sin(2pi x)
+## Cosine array
+yc = np.cos(2*np.pi*x)                      # cos(2pi x)
+
+#plt.xlim(0,2.7)                             # Extend the horizontal plot range (before plotting) to create room for the legend box
+plt.ylim(-1.05,1.4)                             # Extend the horizontal plot range (before plotting) to create room for the legend box
+
+line_sin, = plt.plot(x, ys, label="Sine")   # Plot sine
+line_cos, = plt.plot(x, yc, label="Cosine") # Plot cosine
+
+plt.legend(handles=[line_sin,line_cos])     # Create legend
+
+plt.xlabel('x axis')                        # Label the horizontal axis
+plt.ylabel('y axis')                        # Label the vertical axis
+plt.title('A sine and a cosine')            # Plot title
+plt.grid(True)                              # Plot a grid
+plt.savefig("plot_legend.png")              # Save the plot as png
+#plt.show()                                  # Show the plot to screen
+
