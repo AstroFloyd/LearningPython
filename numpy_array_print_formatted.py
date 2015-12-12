6c58b64c490081ee945d0033c6b6c3fdf7070cf2
AstroFloyd <AstroFloyd@gmail.com>
2015-12-12 15:08:26 +0100

    numpy_array_print_formatted: use random arrays

--- numpy_array_print_formatted.py
+++ numpy_array_print_formatted.py
@@ -5,26 +5,30 @@
 #
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 ## Data array
-dat = np.loadtxt("plot_data.dat")  # Reads the 11x2 array
+dat = np.random.random((11,2))*100
 
-# Print the array directly itself:
+# Print the array itself directly:
 print()
 print(dat)
 
 
 # Print the array contents as text:
 print()
-for i in range(11):
+for i in range(np.shape(dat)[0]):
     print("%4.1f %6.2f" % tuple(dat[i,:]))
 
 
 # For a wide array, you can repeat format groups:
+dat = np.random.random((10,11))*100  # Array of random values between 0 and 100
 print()
-for i in range(2):
-    print(("%6.2f"*4 + "%7.2f"*6 + "%8.2f") % tuple(dat[:,i]))
+print(dat)  # Lines get truncated on the screen and aredifficult to read
+print()
+for i in range(10):
+    print((4*"%6.2f"+7*"%9.4f") % tuple(dat[i,:]))
 
+
+    
 print()
 
