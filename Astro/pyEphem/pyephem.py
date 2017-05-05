bf88648e87005bdb9f6ab4704ddbe4a85a2be032
AstroFloyd <AstroFloyd@gmail.com>
2017-05-05 17:04:12 +0200

    Make script self contained

--- Astro/pyEphem/pyephem.py
+++ Astro/pyEphem/pyephem.py
@@ -1,10 +1,14 @@
+#!/bin/env python3
 # coding: utf-8
+
 import ephem
+import numpy as np
+
 utrecht = ephem.city('Utrecht')
 s = ephem.Sun(utrecht)
-sp = deg2rad((180.,90.-30.))
-print(s.az,s.alt, rad2deg((sp[0],sp[1])))
+sp = np.deg2rad((180.,90.-30.))
+print(s.az,s.alt, np.rad2deg((sp[0],sp[1])))
 
 ang = ephem.separation((s.az,s.alt),sp)
-print(rad2deg(ang), cos(ang), 1000*cos(ang))
+print(np.rad2deg(ang), np.cos(ang), 1000*np.cos(ang))
 
