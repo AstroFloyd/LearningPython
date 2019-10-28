0b26c35449db498fb0ec68cd606171680a5216ab
AstroFloyd <AstroFloyd@gmail.com>
2019-10-28 17:16:23 +0100

    Fitting/numpy.polyfit: tidy up, comment out less useful code, but keep it as an example.

--- Fitting/numpy.polyfit.py
+++ Fitting/numpy.polyfit.py
@@ -15,18 +15,23 @@ import matplotlib.pyplot as plt
 import math as m
 
 
-# Function for computing residuals:
+# Function to be optimised: coefficients as a vector + indipendent variable(s).  Needed for leastsq() and
+# least_squares().
 def optFun1(coefs, x):
     return coefs[0] + coefs[1] * x + coefs[2] * x**2
 
+# Function to be optimised: independent variable(s) + individual coefficients.  Needed for curve_fit.
 def optFun2(x, a,b,c):
     return a + b * x + c * x**2
 
+# Function to compute residuals, for leastsq() and least_squares(), without sigma.
 def resFun1(coefs, x, y):
     return optFun1(coefs, x)  -  y
 
+# Function to compute residuals, for leastsq() and least_squares(), with sigma.
 def resFun2(coefs, x, y, sigmas):
     return (optFun1(coefs, x)  -  y)/sigmas
+
 
 
 #plt.style.use('dark_background')        # Invert colours
@@ -47,8 +52,10 @@ y = f(x) + errors
 #plt.errorbar(x, y, yerr=sigma, fmt='ro')  # Plot red circles with constant error bars
 plt.errorbar(x, y, yerr=errors, fmt='ro')  # Plot red circles with actual error bars
 
-sigmas = np.ones(len(y))*sigma
-sigmas = np.ones(len(y))
+#sigmas = np.ones(len(y))        # Use constant measurement errors, == 1.
+sigmas = np.ones(len(y))*sigma   # Use constant measurement errors, == sigma.
+#sigmas = errors                  # Use measurement errors == actual error!
+
 
 # Fit data and plot fit:
 print("\nSimple fit, no errors:")
@@ -56,22 +63,22 @@ coefs = np.polyfit(x, y, 2)
 print(coefs)
 
 
-print("\nFit without errors, with variance/covariance matrix:")
+print("\nFit with polyfit(), without errors, with variance/covariance matrix:")
 coefs, varCov = np.polyfit(x, y, 2, cov=True, w=1/sigmas)
 #print(coefs)
 #print(varCov)
 #print()
 print("Coefficients:")
 for iCoef in range(3):
-    print(iCoef+1,":", coefs[iCoef], "+-", m.sqrt(varCov[iCoef][iCoef]))
+    print(" ", iCoef+1,":", coefs[iCoef], "+-", m.sqrt(varCov[iCoef][iCoef]))
     
 
 
-print("\nFit without errors, with chi squared:")
+print("\nFit with polyfit(), without errors, with chi squared:")
 coefs, resids, rank, singular_values, rcond = np.polyfit(x, y, 2, full=True)
 #print(resids)
 print("coefficients: ", coefs)
-print("residuals: ", resids)
+#print("residuals: ", resids)
 Chi2 = resids[0]                    # Chi^2
 redChi2 = Chi2/(len(x)-rank)        # Reduced Chi^2 = Chi^2 / (n-m)
 print("Chi2: ", Chi2)
@@ -82,37 +89,20 @@ print("Red. Chi2: ", redChi2)
 
 
 
-if False:
-    print("\n\nFit with constant errors:")
-    coefs, resids, rank, singular_values, rcond = np.polyfit(x, y, 2, w=1/sigmas, full=True)
-    print("coefficients: ", coefs)
-    Chi2 = resids[0]                    # Chi^2
-    redChi2 = Chi2/(len(x)-rank)        # Reduced Chi^2 = Chi^2 / (n-m)
-    print("Chi2: ", Chi2)
-    print("Red. Chi2: ", redChi2)
-    
-    coefs, varCov = np.polyfit(x, y, 2, cov=True, w=1/sigmas)
-    print("\nCoefficients:")
-    for iCoef in range(3):
-        print(iCoef+1,":", coefs[iCoef], "+-", m.sqrt(varCov[iCoef][iCoef]))
-        
-        
-        
-    print("\n\nFit with actual errors:")
-    coefs, resids, rank, singular_values, rcond = np.polyfit(x, y, 2, w=1/errors, full=True)
-    print("coefficients: ", coefs)
-    Chi2 = resids[0]                    # Chi^2
-    redChi2 = Chi2/(len(x)-rank)        # Reduced Chi^2 = Chi^2 / (n-m)
-    print("Chi2: ", Chi2)
-    print("Red. Chi2: ", redChi2)
-    
-    coefs, varCov = np.polyfit(x, y, 2, cov=True, w=1/errors)
-    print("\nCoefficients:")
-    for iCoef in range(3):
-        print(iCoef+1,":", coefs[iCoef], "+-", m.sqrt(varCov[iCoef][iCoef]))
-    
 
+print("\n\nFit with polyfit(), with errors:")
+coefs, resids, rank, singular_values, rcond = np.polyfit(x, y, 2, w=1/sigmas, full=True)
+#print("coefficients: ", coefs)
+Chi2 = resids[0]                    # Chi^2
+redChi2 = Chi2/(len(x)-rank)        # Reduced Chi^2 = Chi^2 / (n-m)
+print("Chi2: ", Chi2)
+print("Red. Chi2: ", redChi2)
 
+coefs, varCov = np.polyfit(x, y, 2, cov=True, w=1/sigmas)
+print("Coefficients:")
+for iCoef in range(3):
+    print(" ", iCoef+1,":", coefs[iCoef], "+-", m.sqrt(varCov[iCoef][iCoef]))
+        
 
 # Plot the (last) fit:
 xn = np.linspace(0, 2, 200)
@@ -120,6 +110,8 @@ yn = np.polyval(coefs, xn)
 plt.plot(xn, yn)
 
 redChi20 = redChi2
+
+
 
 
 
@@ -131,23 +123,23 @@ res = least_squares(resFun2, x0, args=(x, y, sigmas), method='lm')
 #print('res: ', res)
 
 print('Success:      ', res.success)
-print('Cost:         ', res.cost)
+#print('Cost:         ', res.cost)  # = Chi^2/2 !
 #print('Optimality:   ', res.optimality)
-print('Coefficients: ', res.x)
+#print('Coefficients: ', res.x)
 #print('Grad:         ', res.grad)
 #print('Residuals:    ', res.fun)
 
-dCoefs = [0,0,0]  # Not sure how to get the uncertainties in the coefficients!
-print("\nCoefficients:")
-for iCoef in range(3):
-    print(iCoef+1,":", coefs[iCoef], "+-", dCoefs[iCoef])
-    
-print()
 #Chi2 = sum(res.fun**2)                      # Chi^2
 Chi2 = res.cost*2  # Same thing!             # Chi^2
 redChi2 = Chi2/(len(x)-len(res.x))           # Reduced Chi^2 = Chi^2 / (n-m)
 print("Chi2: ", Chi2)
 print("Red. Chi2: ", redChi2)
+
+dCoefs = [0,0,0]  # Not sure how to get the uncertainties in the coefficients!
+print("Coefficients:")
+for iCoef in range(3):
+    print(" ", iCoef+1,":", coefs[iCoef], "+-", dCoefs[iCoef])
+    
 
 print('\nDifference in red. Chi^2: ', abs(redChi20-redChi2))
 
@@ -184,7 +176,7 @@ dCoefs = np.sqrt(np.diag(varCov))      # Standard deviations on the coefficients
 
 print("\nCoefficients:")
 for iCoef in range(3):
-    print(iCoef+1,":", coefs[2-iCoef], "+-", dCoefs[2-iCoef])  # Reverse order w.r.t. polyfit
+    print(" ", iCoef+1,":", coefs[2-iCoef], "+-", dCoefs[2-iCoef])  # Reverse order w.r.t. polyfit
     
 print()
 print("Chi2: ", Chi2)
@@ -206,15 +198,6 @@ plt.plot(xn, yn)
 
 
 
-plt.tight_layout()
-#plt.show()
-plt.savefig('numpy.polyfit.png')                 # Save the plot as png
-plt.close()                             # Close the plot in order to start a new one later
-
-
-print()
-
-
 
 
 
@@ -228,21 +211,22 @@ print()
 
 print("\n\nFit with scipy.optimize.curve_fit():")
 x0 = [-3, 0, 5]  # Initial guess for coefficients
-coefs,varCov = curve_fit(optFun2, x, y, p0=x0, sigma=sigmas, method='lm')
-#coefs, varCov, infodict, mesg, ier = curve_fit(optFun2, x, y, p0=x0, sigma=sigmas, method='lm', full_output=True)
-print('coefficients: ', coefs)
-print('variance/covariance: ', varCov)
+#coefs,varCov = curve_fit(optFun2, x, y, p0=x0, sigma=sigmas, method='lm')
+coefs, varCov, infodict, mesg, ier = curve_fit(optFun2, x, y, p0=x0, sigma=sigmas, method='lm', full_output=True)
+print("Success: ", ier)
+#print('coefficients: ', coefs)
+#print('variance/covariance: ', varCov)
 dCoefs = np.sqrt(np.diag(varCov))     # Standard deviations on the coefficients
 
-print("\nCoefficients:")
+print("Coefficients:")
 for iCoef in range(3):
-    print(iCoef+1,":", coefs[2-iCoef], "+-", dCoefs[2-iCoef])  # Reverse order w.r.t. polyfit
+    print(" ", iCoef+1,":", coefs[2-iCoef], "+-", dCoefs[2-iCoef])  # Reverse order w.r.t. polyfit
     
 print()
-resids = optFun2(x, *coefs) - y  # Residuals
-Chi2    = sum(resids**2)               # Chi^2
-redChi2 = Chi2/(len(x)-len(coefs))     # Reduced Chi^2 = Chi^2 / (n-m)
-print("Chi2: ", Chi2, res.cost*2)
+resids  = (optFun2(x, *coefs) - y)/sigmas   # Weighted residuals
+Chi2    = sum(resids**2)                    # Chi^2
+redChi2 = Chi2/(len(x)-len(coefs))          # Reduced Chi^2 = Chi^2 / (n-m)
+print("Chi2: ", Chi2)
 print("Red. Chi2: ", redChi2)
 
 print('\nDifference in red. Chi^2: ', abs(redChi20-redChi2))
@@ -253,6 +237,20 @@ print('\nDifference in red. Chi^2: ', abs(redChi20-redChi2))
 xn = np.linspace(0, 2, 200)
 yn = np.polyval(coefs, xn)
 plt.plot(xn, yn)
+
+
+
+
+
+
+
+
+
+plt.tight_layout()
+#plt.show()
+plt.savefig('numpy.polyfit.png')                 # Save the plot as png
+plt.close()                             # Close the plot in order to start a new one later
+
 
 print()
 
