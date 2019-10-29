6af1b17c876db9ad943b6f3d8e4c5bf9f84430a6
AstroFloyd <AstroFloyd@gmail.com>
2019-10-29 16:10:46 +0100

    Fitting/scipy.optimize.curve_fit-multivar: create a mock dataset dependent on several variables, and fit the data to the function.

--- Fitting/scipy.optimize.curve_fit-multivar.py
+++ Fitting/scipy.optimize.curve_fit-multivar.py
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
 
 # Function to be optimised: independent variable(s) + individual coefficients.  Needed for curve_fit.
 def optFun(x, a,b,c):
-    return a + b * x + c * x**2
+    return a * np.sin(b*x[0]) * x[1] + c
+    #return a + b * x + c * x**2
+
 
 
 
@@ -21,18 +23,37 @@ plt.figure(figsize=(12.5,7))             # Set png size to 1250x700; savefig has
 
 
 # Create and plot noisy data:
-trueCoefs = [-5, 1, 3]
-sigma = 1.5
+nDat  = 100
+tDat  = np.linspace(0, 12, nDat) + 6  # Time
+xDat1 = np.sin((tDat-6)/12*np.pi) * np.pi / 4  # for 6-18, 0-pi/4 = 0-45°
+xDat2 = np.random.uniform(0, 1, nDat)
+
+# print(tDat)
+# print(xDat1*57)
+# print(xDat2)
+
+trueCoefs = [800, 1, 0]
+sigma = 100
 print("True coefficients: ", trueCoefs)
 print("Sigma: ", sigma)
 
-f = np.poly1d(trueCoefs)
-xDat = np.linspace(0, 2, 20)
-errors = sigma*np.random.normal(size=len(xDat))
-yDat = f(xDat) + errors
+errors = sigma*np.random.normal(size=nDat)
+yDat = trueCoefs[0] * np.sin(trueCoefs[1] * xDat1) * xDat2 + trueCoefs[2] + errors
 
-# plt.errorbar(xDat, yDat, yerr=sigma, fmt='ro')  # Plot red circles with constant error bars
-plt.errorbar(xDat, yDat, yerr=errors, fmt='ro')  # Plot red circles with actual error bars
+yDat[yDat < 0] = 0
+
+# print(yDat)
+
+xDat = np.array([xDat1,xDat2])
+
+# print()
+# print(xDat)
+
+# plt.errorbar(tDat, yDat, yerr=sigma, fmt='ro')  # Plot red circles with constant error bars
+plt.errorbar(tDat, yDat, yerr=errors, fmt='ro')  # Plot red circles with actual error bars
+
+plt.plot(tDat, xDat1*57)
+plt.plot(tDat, xDat2*100)
 
 # Array of measurement errors:
 #sigmas = np.ones(len(yDat))        # Use constant measurement errors, == 1.
@@ -43,8 +64,7 @@ sigmas = np.ones(len(yDat))*sigma   # Use constant measurement errors, == sigma.
 
 
 print("\nFit with scipy.optimize.curve_fit():")
-coef0 = [-3, 0, 5]  # Initial guess for coefficients
-#coefs,varCov = curve_fit(optFun, xDat, yDat, p0=coef0, sigma=sigmas, method='lm')
+coef0 = [500, 2, -1]  # Initial guess for coefficients
 coefs, varCov, infodict, mesg, ier = curve_fit(optFun, xDat, yDat, p0=coef0, sigma=sigmas, method='lm', full_output=True)
 print("Success: ", ier)
 #print("Success: ", mesg)
@@ -55,27 +75,28 @@ print('coefficients: ', coefs)
 dCoefs = np.sqrt(np.diag(varCov))     # Standard deviations on the coefficients
 
 resids  = (optFun(xDat, *coefs) - yDat)/sigmas    # Weighted residuals
-Chi2    = sum(resids**2)                    # Chi^2
-redChi2 = Chi2/(len(xDat)-len(coefs))          # Reduced Chi^2 = Chi^2 / (n-m)
+Chi2    = sum(resids**2)                          # Chi^2
+redChi2 = Chi2/(len(yDat)-len(coefs))             # Reduced Chi^2 = Chi^2 / (n-m)
 print("Chi2: ", Chi2)
 print("Red. Chi2: ", redChi2)
 print("Coefficients:")
 for iCoef in range(3):
-    print(" ", iCoef+1,":", coefs[2-iCoef], "+-", dCoefs[2-iCoef])  # Reverse order w.r.t. polyfit
+    print(" ", iCoef+1,":", coefs[iCoef], "+-", dCoefs[iCoef])  # Reverse order w.r.t. polyfit
 
 
 
 
 # Plot the fit:
-xFit = np.linspace(0, 2, 200)
-yFit = np.polyval([coefs[2],coefs[1],coefs[0]], xFit)
-#plt.plot(xFit, yFit)
+yFit = optFun(xDat, *coefs)
+plt.plot(tDat, yFit)
 
 plt.tight_layout()
 #plt.show()
 plt.savefig('scipy.optimize.curve_fit-multivar.png')                 # Save the plot as png
 plt.close()                             # Close the plot in order to start a new one later
 
+#print(xFit)
+#print(yFit)
 
 print()
 
