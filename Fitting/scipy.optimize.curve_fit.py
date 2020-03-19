34fc0e2cb5a48c268ba2abdcf235b25595d309d3
AstroFloyd <AstroFloyd@gmail.com>
2020-03-19 20:46:44 +0100

    scipy.optimize.curve_fit: a few minor improvements.
    * scipy.optimize.curve_fit: a few minor improvements:
      + plot the true function
      + unpack the coefficients when calling optFun()

--- Fitting/scipy.optimize.curve_fit.py
+++ Fitting/scipy.optimize.curve_fit.py
@@ -31,7 +31,10 @@ print("Sigma: ", sigma)
 
 xDat = np.linspace(0, 2, 20)
 errors = sigma*np.random.normal(size=len(xDat))
-yDat = optFun(xDat, trueCoefs[0],trueCoefs[1],trueCoefs[2]) + errors
+yDat = optFun(xDat, *trueCoefs)  # Unpack trueCoefs
+plt.plot(xDat, yDat, ':')
+
+yDat += errors
 
 # plt.errorbar(xDat, yDat, yerr=sigma, fmt='ro')  # Plot red circles with constant error bars
 plt.errorbar(xDat, yDat, yerr=errors, fmt='ro')  # Plot red circles with actual error bars
@@ -52,27 +55,31 @@ coef0 = [-3, 0, 5]  # Initial guess for coefficients
 coefs, varCov, infodict, mesg, ier = curve_fit(optFun, xDat, yDat, p0=coef0, sigma=sigmas, method='lm', full_output=True)
 
 print("Success: ", ier)
-print('coefficients: ', coefs)
-print('variance/covariance: ', varCov)
-dCoefs = np.sqrt(np.diag(varCov))                # Standard deviations on the coefficients
+print("Message: ", mesg)
+# print(infodict)
+print("coefficients: ", coefs)
+print("variance/covariance: \n", varCov)
 
-resids  = (optFun(xDat, *coefs) - yDat)/sigmas   # Weighted residuals
-Chi2    = sum(resids**2)                         # Chi^2
-redChi2 = Chi2/(len(xDat)-len(coefs))            # Reduced Chi^2 = Chi^2 / (n-m)
+dCoefs  = np.sqrt(np.diag(varCov))                # Standard deviations on the coefficients
+
+resids  = (optFun(xDat, *coefs) - yDat)/sigmas    # Weighted residuals
+Chi2    = sum(resids**2)                          # Chi^2
+redChi2 = Chi2/(len(xDat)-len(coefs))             # Reduced Chi^2 = Chi^2 / (n-m)
 
 print("Chi2: ", Chi2)
 print("Red. Chi2: ", redChi2)
 print("Coefficients:")
-for iCoef in range(3):
-    print(" ", iCoef+1,":", coefs[iCoef], "+-", dCoefs[iCoef])
-    # print(" ", iCoef+1,":", coefs[2-iCoef], "+-", dCoefs[2-iCoef])  # Reverse order w.r.t. polyfit
+for iCoef in range(len(coefs)):
+    # print(" ", iCoef+1,":", coefs[2-iCoef], "±", dCoefs[2-iCoef])  # Reverse order w.r.t. polyfit
+    # print(" ", iCoef+1,":", coefs[iCoef], "±", dCoefs[iCoef])
+    print(" c%1i: %9.5f ± %9.5f " % (iCoef+1,coefs[iCoef], dCoefs[iCoef]))  # Formatted
 
 
 
 
 # Plot the fit:
 xFit = np.linspace(0, 2, 200)
-yFit = optFun(xFit, coefs[0],coefs[1],coefs[2])
+yFit = optFun(xFit, *coefs)  # Unpack coefs
 plt.plot(xFit, yFit)
 
 plt.tight_layout()
